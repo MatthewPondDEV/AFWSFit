94117bd1db6001618c349c5b7276ba8789d8ba53
MatthewPondDEV <matthewpondDEV@gmail.com>
2024-03-24 13:55:39 -0400

    new commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,28 +9,35 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export default function Header() {
   return (
-    <Navbar id='header-nav' variant='dark' expand="lg" sticky="top" className="justify-content-around">
+    <Navbar
+      id="header-nav"
+      variant="dark"
+      expand="lg"
+      sticky="top"
+      className="justify-content-around"
+    >
       <Container>
         <Navbar.Brand href="#home">
-          <Image src={logo} roundedCircle style={{ height: "100px" }} />
+          <Image src={logo} roundedCircle style={{ height: "120px" }} />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Offcanvas
           id={`offcanvasNavbar-expand-md`}
           aria-labelledby={`offcanvasNavbarLabel-expand-md`}
           placement="end"
+          variant='dark'
         >
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`} >
-              <Image src={logo} roundedCircle style={{ height: "100px" }} />
+            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+              <Image src={logo} roundedCircle style={{ height: "120px" }} />
             </Offcanvas.Title>
           </Offcanvas.Header>
           <Offcanvas.Body>
-            <Nav className='m-auto'>
+            <Nav className="m-auto">
               <Nav.Link href="#link">Workouts</Nav.Link>
               <Nav.Link href="#link">Nutrition</Nav.Link>
               <Nav.Link href="#link">Shop</Nav.Link>
@@ -38,18 +45,10 @@ export default function Header() {
               <Nav.Link href="#link">FAQs</Nav.Link>
             </Nav>
             <Nav>
-              <NavDropdown
-              title="Account"
-              id={`offcanvasNavbarDropdown-expand-lg`}
-              className='my-auto me-2'
-              menuRole='menu'
-              drop='down-centered'
-            >
-              <NavDropdown.Item href="#action3">Login</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action4">Register</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className=''><i class="bi bi-bag"></i> Cart</Nav.Link>
+              <Nav.Link>Login</Nav.Link>
+              <Nav.Link className="">
+                <i class="bi bi-bag"></i> Cart
+              </Nav.Link>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,14 +1,49 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/Col";
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import Carousel from "react-bootstrap/Carousel";
+import carouselBg from '../assets/CarouselBG.png'
 
 export default function IndexPage() {
-
-    return (
-        <>
-       
-        </>
-    )
+  return (
+    <Container>
+      <Row>
+        <Col xs={12}>
+          <Carousel fade>
+            
+            <Carousel.Item className='w-100'>
+              <Image src={carouselBg} style={{width: '100%', height: '700px'}}/>
+              <Carousel.Caption>
+                <h3>First slide label</h3>
+                <p>
+                  Nulla vitae elit libero, a pharetra augue mollis interdum.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+               <Image src={carouselBg} style={{width: '100%', height: '700px'}}/>
+              <Carousel.Caption>
+                <h3>Second slide label</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+             <Image src={carouselBg} style={{width: '100%', height: '700px'}}/>
+              <Carousel.Caption className='text-white'>
+                <h3>Third slide label</h3>
+                <p>
+                  Praesent commodo cursus magna, vel scelerisque nisl
+                  consectetur.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
